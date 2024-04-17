b7c15e7ee3fb18caf1d4f93a693efe1a03e7eb8d
anthonyjt21 <anthonyjt_21@hotmail.com>
2024-04-17 16:17:27 -0600

    add all posts
    add all posts

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,2 +1,40 @@
-function AllPostsPage() {}
+import AllPosts from '../../components/posts/all-posts';
+
+const DUMMY_POSTS = [
+  {
+    slug: 'getting-started-with-nextjs',
+    title: 'Getting Started with NextJs',
+    image: 'getting-started-nextjs.png',
+    excerpt:
+      'Nextjs is a React framework, it make building fullstack ap  ps easy and fun!',
+    date: '2021-01-01',
+  },
+  {
+    slug: 'getting-started-with-nextjs2',
+    title: 'Getting Started with NextJs2',
+    image: 'getting-started-nextjs2.png',
+    excerpt:
+      'Nextjs is a React framework, it make building fullstack ap  ps easy and fun!',
+    date: '2021-01-01',
+  },
+  {
+    slug: 'getting-started-with-nextjs3',
+    title: 'Getting Started with NextJs3',
+    image: 'getting-started-nextjs3.png',
+    excerpt:
+      'Nextjs is a React framework, it make building fullstack ap  ps easy and fun!',
+    date: '2021-01-01',
+  },
+  {
+    slug: 'getting-started-with-nextjs4',
+    title: 'Getting Started with NextJs4',
+    image: 'getting-started-nextjs4.png',
+    excerpt:
+      'Nextjs is a React framework, it make building fullstack ap  ps easy and fun!',
+    date: '2021-01-01',
+  },
+];
+function AllPostsPage() {
+  return <AllPosts posts={DUMMY_POSTS} />;
+}
 export default AllPostsPage;
